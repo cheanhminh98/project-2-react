00f4eef110a1b9e593a10bad2a6c3c695f927372
AnhMinh <cheanhminh1401@gmail.com>
2020-08-08 19:50:42 +0700

    Show todo list

---
+++ src/components/Task-Item.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class TaskItem extends Component{
+	render(){
+        var {task, index} = this.props; 
+		return(
+			<tr>
+                <td>{index+1}</td>
+                <td>{task.name}</td>
+                <td className="text-center">
+                    <span className={task.status === true ? "label label-success" : "label label-warning"}>
+                        {task.status === true ? "Đã hoàn thành" : "Đang thực hiện"}
+                    </span>
+                </td>
+                <td className="text-center">
+                    <button type="button" className="btn btn-warning">
+                        <span className="fas fa-edit mr-5"></span>Sửa
+                    </button>
+                    &nbsp;
+                    <button type="button" className="btn btn-danger">
+                        <span className="fa fa-trash mr-5"></span>Xóa
+                    </button>
+                </td>
+            </tr>
+		);
+	}
+}
+
+export default TaskItem;
